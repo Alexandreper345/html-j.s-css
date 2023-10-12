9a7d0f192d35f5e11a7db44167085a3e53d4e298
Alexandreper345 <138054329+Alexandreper345@users.noreply.github.com>
2023-10-12 17:57:52 -0300

    primeiro projeto de ver horario

--- modelo de determinada horas/javinha.js
+++ modelo de determinada horas/javinha.js
@@ -1,2 +1,20 @@
-var msg = window.document.getElementById('msg')
-var foto = window.document.getElementById('foto')
+function carrega() {
+    
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora são ${hora} horas`
+
+    if (hora >= 0 && hora <12){
+        img.src = 'dia.png'
+        document.body.style.background = '#d1d22e'
+
+    }else if (hora >= 12 && hora <= 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#808000'    
+    }else {
+        img.src = 'noite.png'
+        document.body.style.background = '#4747eb'
+    }
+}
